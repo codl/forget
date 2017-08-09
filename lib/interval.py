b428788fc0a195d82f325ea189b1bd2daed1e827
codl <codl@codl.fr>
2017-08-09 09:22:34 +0200

    oops precedence
    interestingly enough it doesn't matter here but, let's be correct

--- lib/interval.py
+++ lib/interval.py
@@ -11,7 +11,7 @@ SCALES = [
     # me, wise:
         mean((31,
             mean((29 if year % 400 == 0
-                        or year % 100 != 0 and year % 4 == 0
+                        or (year % 100 != 0 and year % 4 == 0)
                     else 28
                     for year in range(400)))
             ,31,30,31,30,31,31,30,31,30,31))
@@ -20,7 +20,7 @@ SCALES = [
     # you, a fool: ok. a year is 365.25 days. happy?
     # me, wise: absolutely not
         mean((366 if year % 400 == 0
-                or year % 100 != 0 and year % 4 == 0
+                or (year % 100 != 0 and year % 4 == 0)
             else 365
             for year in range(400)))
         )),
