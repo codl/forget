dd212ce1c382f51da438e45c500a55629f900593
codl <codl@codl.fr>
2017-08-11 16:41:31 +0200

    process all assets thru doit

--- dodo.py
+++ dodo.py
@@ -21,6 +21,30 @@ def task_gen_logo():
             'file_dep': ['assets/logotype.png'],
         }
 
+def task_copy_icon():
+    import shutil
+    return {
+            'actions': [lambda: shutil.copy('assets/icon.png', 'static/icon.png')],
+            'targets': ['static/icon.png'],
+            'file_dep': ['assets/icon.png'],
+    }
+
+def task_minify_css():
+    """minify css"""
+
+    from csscompressor import compress
+
+    def minify():
+        with open('assets/styles.css') as in_:
+            with open('static/styles.css', 'w') as out:
+                out.write(compress(in_.read()))
+
+    return {
+            'actions': [minify],
+            'targets': ['static/styles.css'],
+            'file_dep': ['assets/styles.css'],
+        }
+
 if __name__ == '__main__':
     import doit
     doit.run(globals())
