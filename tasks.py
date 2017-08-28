2823f5fb76d1020ed9e875ba0fcb4a6a2f873d43
codl <codl@codl.fr>
2017-08-28 17:19:07 +0200

    celery statsd

--- tasks.py
+++ tasks.py
@@ -15,6 +15,7 @@ import json
 from kombu import Queue
 import random
 import version
+import celery_statsd
 
 app = Celery('tasks', broker=flaskapp.config['CELERY_BROKER'], task_serializer='pickle')
 app.conf.task_queues = (
