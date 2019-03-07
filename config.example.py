89a718cfea4112d1649e74ebe665e2b9e56b961e
codl <codl@codl.fr>
2019-03-08 00:58:23 +0100

    document config.example.py better

--- config.example.py
+++ config.example.py
@@ -8,7 +8,7 @@ copy this file to config.py before editing
 DATABASE URI
 
 determines where to connect to the database
-see http://docs.sqlalchemy.org/en/latest/core/engines.html#database-urls for syntax
+see <http://docs.sqlalchemy.org/en/latest/core/engines.html#database-urls> for syntax
 only postgresql with psycopg2 driver is officially supported
 """
 # SQLALCHEMY_DATABASE_URI='postgresql+psycopg2:///forget'
@@ -16,7 +16,7 @@ only postgresql with psycopg2 driver is officially supported
 """
 REDIS URI
 
-see https://redis-py.readthedocs.io/en/latest/#redis.ConnectionPool.from_url
+see <https://redis-py.readthedocs.io/en/latest/#redis.ConnectionPool.from_url>
 for syntax reference
 """
 # REDIS_URI='redis://'
@@ -24,24 +24,35 @@ for syntax reference
 """
 TWITTER CREDENTIALS
 
-get these at apps.twitter.com
-blah
+Apply for api keys on the developer portal <https://developer.twitter.com/en/apps>
+Twitter locked me out of it so I can't guide you more than that. Sorry.
 """
 # TWITTER_CONSUMER_KEY='vdsvdsvds'
 # TWITTER_CONSUMER_SECRET='hjklhjklhjkl'
 
 """
-this will be necessary so we can tell twitter where to redirect
+SERVER ADDRESS
+
+This is the address at which forget will be reached.
+External services will redirect to this address when logging in.
 """
 # SERVER_NAME="localhost:5000"
-
-
 # HTTPS=True
 
-# SENTRY_DSN='https://foo:bar@sentry.io/69420'
 
 """
-you can also use any config variable that flask expects here, such as
+SENTRY
+
+If you want to send exceptions to sentry, enter your sentry DSN here
+"""
+# SENTRY_DSN=
+
+"""
+ADVANCED FLASK CONFIG
+
+you can also use any config variable that flask expects here
+A list of these config variables is available here:
+<http://flask.pocoo.org/docs/1.0/config/#builtin-configuration-values>
 """
 # SESSION_COOKIE_SECURE=True
 # DEBUG=True
